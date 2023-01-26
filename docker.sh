45d0e1a1433a1b7d8377e3eb5b620a00e8ed0e30
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2023-01-26 23:02:52 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt update -y
 sudo apt install -y docker.io
-sudo apt update -y
-sudo apt get install docker-compose-plugin 2>/dev/null
+sudo apt-get update -y
+sudo apt-get install docker-compose-plugin 2>/dev/null
