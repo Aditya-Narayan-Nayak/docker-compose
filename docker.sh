a5574f33f09558cc50c076b4fed67933fac349af
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2023-01-27 00:00:22 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-sudo apt-get install curl
-sudo apt-get install gnupg
-sudo apt-get install ca-certificates
-sudo apt-get install lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginsudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginlinux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y -q
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt update -y
+sudo apt install -y docker.io
+curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
