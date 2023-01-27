a7723d9724cf35f3d1c166039c456f91f6cdf2bf
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2023-01-27 14:51:28 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,3 +4,10 @@ sudo apt install -y docker.io
 curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+git clone https://github.com/django-cms/django-cms-quickstart.git
+cd django-cms-quickstart
+docker compose build web
+docker compose up -d database_default
+docker compose run web python manage.py migrate
+docker compose run web python manage.py createsuperuser
+docker compose up -d
